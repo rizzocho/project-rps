c787b033372dd7d40b726227728320164b0f8b04
rizzocho <98249535+rizzocho@users.noreply.github.com>
2024-10-21 21:42:06 -0700

    Create functions getHumanChoiceand playRound

--- script.js
+++ script.js
@@ -13,5 +13,40 @@ function getComputerChoice() {
   } return computerChoice;
 }
 
-getComputerChoice();
+function getHumanChoice() {
+  let humanEntry = prompt('rock, paper, scissors','');
+  let humanChoice = humanEntry.toLowerCase()
+  
+  if (humanChoice == 'rock' || humanChoice == 'paper' || humanChoice == 'scissors') {
+    console.log('you picked', humanChoice);
+    return humanChoice;
 
+  } else {
+    console.log('invalid');
+    getHumanChoice();
+  }
+}
+
+
+let humanScore = 0;
+let computerScore = 0;
+
+function playRound(humanChoice, computerChoice) {
+  console.log('computer picked', computerChoice);
+  if (humanChoice == computerChoice) {
+    console.log('It\'s a tie!');
+  } else if ((humanChoice == 'rock' && computerChoice == 'scissors') || (humanChoice == 'paper' && computerChoice == 'rock') || (humanChoice == 'scissors' && computerChoice == 'paper')) {
+    console.log('You WIN!')
+    humanScore++
+    
+  } else {
+    console.log('You LOSE!')
+    computerScore++
+
+  } console.log(`You have ${humanScore} points, Computer has ${computerScore} points`);
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
